78dba0c65a1ea4bac765f9266bdb0c040cad62a0
AmberSnow12 <55108284+AmberSnow12@users.noreply.github.com>
2019-09-18 17:50:30 -0400

    Add files via upload

---
+++ intro to R.R
@@ -0,0 +1,17 @@
+2+3
+x<-4
+x+2
+x+2
+my_num <-2/2
+my_char <-"hi!"
+my_bool <- TRUE
+typeof(my_num) 
+typeof(my_char)
+typeof(my_bool)
+my_nan <-0/0
+my_nan
+install.packages("tidyverse")
+library(tidyverse)
+#creates a null variable 
+my_nan <-0/0
+help(type)
